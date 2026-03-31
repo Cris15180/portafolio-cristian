fb0a80030182554ccdd2ce1f123c126a3a5b7ed3
Cris15180 <gabocampos582@gmail.com>
2026-03-31 16:38:00 -0600

    se crea un nav lateral, al igual, se realizan cambios en el boton de contacto, se añaden animaciones

--- js/script.js
+++ js/script.js
@@ -15,3 +15,26 @@ gsap.from(".sobre-mi", {
   duration: 1,
   stagger: 1
 });
+
+function copiarCorreo() {
+    const email = "gabocampos582@gmail.com";
+    const boton = document.getElementById("btnCorreo");
+    const textoOriginal = boton.innerText;
+
+    // 1. Copia el texto al portapapeles
+    navigator.clipboard.writeText(email).then(() => {
+      
+      // 2. Cambia el texto del botón
+      boton.innerText = "¡Copiado!";
+      boton.style.backgroundColor = "#597add"; // Opcional: cambia a color verde
+      
+      // 3. Vuelve al estado original después de 2 segundos (2000 ms)
+      setTimeout(() => {
+        boton.innerText = textoOriginal;
+        boton.style.backgroundColor = ""; // Vuelve al color original
+      }, 2000);
+
+    }).catch(err => {
+      console.error('Error al copiar: ', err);
+    });
+  }
